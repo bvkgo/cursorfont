fe7f51d332810ab4fc2f0b53616097c53c1e0edd
BVK Chaitanya <bvk@localhost.localdomain>
2024-05-15 10:09:28 -0500

    Added X11 cursor name codes and the go:generate script.

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Package cursorfont defines Go constants for the cursor names from
+/usr/include/X11/cursorfont.h file.
+
+Cursor names are the same as their C language identifiers, including the
+underscores, XC_ prefix and capitalization.
+*/
+package cursorfont
+
+//go:generate go run gen.go
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bvkgo/cursorfont
+
+go 1.21.4
+
+require github.com/jezek/xgb v1.1.1
